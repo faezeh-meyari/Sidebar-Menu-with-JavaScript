299b0e0f0db5387841ce87b53ca3224761124871
faezehmeyari <faezehmeyari203@gmail.com>
2026-02-20 10:57:14 +0100

    Responsive the page, Add dashboard to the page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const sidebar = document.querySelector(".sidebar");
-const sidebarToggleBtn = document.querySelector(".sidebar-toggle");
+const sidebarToggleBtn = document.querySelectorAll(".sidebar-toggle");
 const themeToggleBtn = document.querySelector(".theme-toggle");
 const themeIcon = themeToggleBtn.querySelector('.theme-icon');
-
+const searchForm = document.querySelector('.search-form');
+const menuItems = document.querySelectorAll(".menu-link");
+const mainContent = document.querySelector('.main-content');
 
 const updateThemeIcon = () => {
     const isDark = document.body.classList.contains("dark-theme");
@@ -18,14 +20,48 @@ const shouldUseDarkTheme = saveTheme === "dark" || (!saveTheme && systemPrefersD
 document.body.classList.toggle("dark-theme" , shouldUseDarkTheme);
 updateThemeIcon();
 
-sidebarToggleBtn.addEventListener("click", () => {
+//Toggle sidebar collapsed state on buttons click
+
+/*sidebarToggleBtn.addEventListener("click", () => {
     sidebar.classList.toggle("collapsed");
     updateThemeIcon();
-});
+});*/
+sidebarToggleBtn.forEach(btn => {
+    btn.addEventListener("click" , () =>{
+        sidebar.classList.toggle("collapsed");
+        updateThemeIcon();
+    })
+})
 
+//Expand the sidebar when the search form is clicked
+searchForm.addEventListener("click", (e) => {
+    if(sidebar.classList.contains("collapsed")) {
+        sidebar.classList.remove("collapsed");
+        searchForm.querySelector("input").focus();
+    }
+})
 
+//Toggle between themes on theme button click
 themeToggleBtn.addEventListener("click", () => {
     const isDark = document.body.classList.toggle("dark-theme");
     localStorage.setItem("theme", isDark ? "dark" : "light");
     updateThemeIcon()
-})
+});
+if(window.innerWidth > 768){
+    sidebar.classList.remove("collapsed");
+
+}
+
+//Active clicked link
+menuItems.forEach(menuItem => {
+    menuItem.addEventListener("click" ,function(e) {
+        e.preventDefault(); // Prevent from refreshing the link
+
+        menuItems.forEach( link => {
+            link.classList.remove("active");
+        });
+        this.classList.add("active");
+    });
+});
+
+
